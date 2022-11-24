73e8f2918ab8fae0efa81f19885b232b2dd4d94d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-24 02:17:45 +0000

    Adding tarfile member sanitization to extractall()

--- sciencebeam_trainer_delft/sequence_labelling/engines/wapiti_install.py
+++ sciencebeam_trainer_delft/sequence_labelling/engines/wapiti_install.py
@@ -29,7 +29,26 @@ def install_wapiti_and_get_path_or_none(
     LOGGER.debug('local_file: %s', local_file)
     LOGGER.debug('extracting to: %s', extracted_directory)
     with tarfile.open(local_file, mode='r') as tar:
-        tar.extractall(extracted_directory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, extracted_directory)
     extracted_files = os.listdir(extracted_directory)
     wapiti_source_directory = extracted_directory
     if len(extracted_files) == 1:
